ddf15b2530434a893494639c14a1fb94a032c1ce
ys8976 <1qkakzk@naver.com>
2023-01-13 22:53:22 +0900

    second commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <div class="flex ">
+                <div class="basis-1/6 h-screen border-r-2 border-solid py-5">
+
+                    User List<br>
+                    User List<br>
+                    User List<br>
+                    User List<br>
+
+
+                </div>
+                <div class="basis-5/6 h-screen py-5 px-5">
+                    <div class="flex-1">Chat Area
+                    </div>
+                    <div class="absolute  bottom-8 w-4/6">
+                        <input class="border-2 border-solid rounded border-gray-600 w-full" type="text">
+                    </div>
+                </div>
+            </div>
+        </template>
+    </AuthenticatedLayout>
+</template>
